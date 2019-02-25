89b51497fed9d0e30605625649c7a5044be80859
zhaozichuan <zhaozichuan@sina.com>
2019-02-25 17:24:33 +0800

    1 step

--- app/models/Company.java
+++ app/models/Company.java
@@ -1,8 +1,14 @@
 package models;
 
+import play.data.format.Formats;
 import play.data.validation.Constraints;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import io.ebean.annotation.CreatedTimestamp;
 
 
 
@@ -10,12 +16,35 @@ import javax.persistence.Entity;
  * Company entity managed by Ebean
  */
 @Entity 
-public class Company extends BaseModel {
+public class Company{
 
     private static final long serialVersionUID = 1L;
-
-    @Constraints.Required
-    public String name;
-
+   
+    @Id
+    public String comid; //单位id
+    
+    public String name;  //单位名称
+   
+    public String wxid;  //微信id
+    public String addshort; //地址简称
+    public String addlong;  //详细地址
+    public float  lat;   //地址精度
+    public float  ing;   //地址纬度
+    public String tel;   //电话
+    /**
+     * 创建时间
+     */
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedTimestamp
+    public Date createtime;
+    public int  status;  //单位状态
+    
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedTimestamp
+    public Date checktime; //审核时间
+        
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedTimestamp
+    public Date modifytime; //修改时间
 }
 
