ac77448931e1dbbce4aebcb4e260ca463f7bfd59
zhaozichuan <zhaozichuan@sina.com>
2019-04-23 17:28:34 +0800

    auto

---
+++ app/zzc/Log4jTest.java
@@ -0,0 +1,22 @@
+package zzc;
+
+import org.apache.log4j.Logger;
+
+//import org.apache.log4j.Logger;
+
+
+public class Log4jTest {
+   public static Logger logger1 = Logger.getLogger(Log4jTest.class);
+ 
+   
+   
+   public static void main(String[] args) {
+       //logger1
+       logger1.trace("我是logger1，trace");
+       logger1.debug("我是logger1，debug");
+       logger1.info("我是logger1，info");
+       logger1.warn("我是logger1，warn");
+       logger1.error("我是logger1，error");
+       logger1.fatal("我是logger1，fatal");
+   }
+}
--- app/zzc/MyActionCreator.java
+++ app/zzc/MyActionCreator.java
@@ -5,9 +5,8 @@ import play.mvc.Http;
 import play.mvc.Http.Context;
 import play.mvc.Result;
 import java.util.concurrent.CompletionStage;
-
 import java.lang.reflect.Method;
-
+import zzc.Log4jTest;
 public class MyActionCreator implements play.http.ActionCreator {
     @Override
     public Action createAction(Http.Request request, Method actionMethod) {
@@ -16,11 +15,15 @@ public class MyActionCreator implements play.http.ActionCreator {
 //            public CompletionStage<Result> call(Http.Request req) {
 //                return delegate.call(req);
 //            }
-
+        	
+        	
 			@Override
 			public CompletionStage<Result> call(Context ctx) {
 				// TODO Auto-generated method stub
-				System.out.println("MyActionCreator");
+				System.out.println("MyActionCreator"+ctx.request());
+
+				Log4jTest.logger1.debug("我是logger1，debug");
+				
 				return delegate.call(ctx);
 			}
         };
